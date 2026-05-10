ba639add49ec05b7ff8a6f9fa6c9e04f5cb1aa99
Rasha202 <wcnh1333@gmail.com>
2026-05-10 20:20:43 +0300

    Update list.js

--- src/resources/list.js
+++ src/resources/list.js
@@ -1,36 +1,36 @@
 const resourceListSection = document.querySelector("#resource-list-section");
 
 function createResourceArticle(resource) {
-  const article = document.createElement("article");
+    const article = document.createElement("article");
 
-  const title = document.createElement("h2");
-  title.textContent = resource.title;
+    const title = document.createElement("h2");
+    title.textContent = resource.title;
 
-  const description = document.createElement("p");
-  description.textContent = resource.description;
+    const description = document.createElement("p");
+    description.textContent = resource.description;
 
-  const link = document.createElement("a");
-  link.textContent = "View Resource & Discussion";
-  link.href = `details.html?id=${resource.id}`;
+    const link = document.createElement("a");
+    link.textContent = "View Resource & Discussion";
+    link.href = `details.html?id=${resource.id}`;
 
-  article.appendChild(title);
-  article.appendChild(description);
-  article.appendChild(link);
+    article.appendChild(title);
+    article.appendChild(description);
+    article.appendChild(link);
 
-  return article;
+    return article;
 }
 
 async function loadResources() {
-  const res = await fetch("./api/index.php");
-  const data = await res.json();
+    const response = await fetch("./api/index.php");
+    const result = await response.json();
 
-  resourceListSection.innerHTML = "";
+    resourceListSection.innerHTML = "";
 
-  if (data.success) {
-    data.data.forEach(resource => {
-      resourceListSection.appendChild(createResourceArticle(resource));
-    });
-  }
+    if (result.success) {
+        result.data.forEach(resource => {
+            resourceListSection.appendChild(createResourceArticle(resource));
+        });
+    }
 }
 
 loadResources();
