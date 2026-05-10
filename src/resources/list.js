01ce83f900a8b9f390ca11ec3f6c1566049ed048
Rasha202 <wcnh1333@gmail.com>
2026-05-10 17:57:50 +0300

    Update list.js

--- src/resources/list.js
+++ src/resources/list.js
@@ -1,15 +1,37 @@
-const resourceListSection = document.getElementById("resource-list-section");
+/*
+  Requirement: Populate the "Course Resources" list page.
+
+  Instructions:
+  1. Link this file to `list.html` using:
+     <script src="list.js" defer></script>
+
+  2. In `list.html`, add id="resource-list-section" to the
+     <section> element that will contain the resource articles.
+
+  3. Implement the TODOs below.
+*/
+
+// --- Element Selections ---
+const resourceListSection = document.querySelector("#resource-list-section");
+
+// --- Functions ---
 
 function createResourceArticle(resource) {
   const article = document.createElement("article");
 
-  article.innerHTML = `
-    <h2>${resource.title}</h2>
-    <p>${resource.description}</p>
-    <a href="details.html?id=${resource.id}">
-      View Resource & Discussion
-    </a>
-  `;
+  const title = document.createElement("h2");
+  title.textContent = resource.title;
+
+  const description = document.createElement("p");
+  description.textContent = resource.description;
+
+  const link = document.createElement("a");
+  link.textContent = "View Resource & Discussion";
+  link.href = `details.html?id=${resource.id}`;
+
+  article.appendChild(title);
+  article.appendChild(description);
+  article.appendChild(link);
 
   return article;
 }
@@ -21,11 +43,12 @@ async function loadResources() {
 
   resourceListSection.innerHTML = "";
 
-  result.data.forEach(resource => {
+  result.data.forEach((resource) => {
     const article = createResourceArticle(resource);
 
     resourceListSection.appendChild(article);
   });
 }
 
+// --- Initial Page Load ---
 loadResources();
